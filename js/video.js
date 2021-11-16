4f83af50fcb8031f92010d235088dbe6fa79f34f
bria559 <80613139+bria559@users.noreply.github.com>
2021-11-16 18:10:30 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,78 @@
+var video =document.querySelector('#player1');
+var vidVolume = document.querySelector('#volume');
+var vidMute = document.querySelector("#mute");
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	vidVolume.innerHTML = (video.volume * 100) + '%';
+	console.log(video.currentTime);
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+vidMute.addEventListener("click", function() {
+	if (video.muted === false) { 
+		video.muted = true;
+		vidMute.innerHTML = 'Unmute';
+ 	}
+	else{
+		video.muted = false;
+		vidMute.innerHTML = 'Mute';
+	}
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add('oldSchool');
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove('oldSchool');
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if(video.currentTime + 15 <= video.duration){
+	video.currentTime = video.currentTime + 15;
+	console.log(video.currentTime);
+	}
+	else{
+		video.load();
+		console.log(video.currentTime);
+	}
+});
+
+document.querySelector("#slider").addEventListener("change", function() {
+	var volRange = document.querySelector("#volume");
+	var volRangeValue = document.querySelector("#slider").value;
+	console.log(volRangeValue);
+	video_volume = volRangeValue / 1000;
+	video.volume = video_volume;
+	volRange.innerHTML = (video.volume *1000) + "%";
+});
+
+// NOT DONE
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playBackRate = video.playBackRate
+	console.log("Old Video speed is " + video.playBackRate);
+	video.playBackRate = 0.05;
+	console.log("New video speed is " + video.playBackRate);
+	//figure out how to make it progressively slower with each click
+	// link to answer should be on last week's js slides
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Old Video speed is " + video.playBackRate);
+	video.playBackRate = 2.0;
+	console.log("New video speed is " + video.playBackRate);
+	//figure out how to make it progressively faster with each click
+	// link to answer should be on last week's js slides
+});
